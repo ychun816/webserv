70c463a67384bc91300e1e84b8cd12f820a59c7a
crytalsith <crystalsithh@gmail.com>
2025-02-28 14:27:28 +0100

    socket non bloquante

--- includes/server/Server.hpp
+++ includes/server/Server.hpp
@@ -5,6 +5,10 @@
 #include <cstring>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <vector>
+#include <deque>
+#include <unistd.h>
+#include <fcntl.h>
 
 class Server
 {
@@ -23,8 +27,10 @@ class Server
 		void	init(std::string &configFile);
 		void	createSocket();
 		void	configSocket();
+		void	runServer();
 	private:
 		std::string 		_configFile;
+		std::deque<int>		_connexions;
 		int					_socketFd;
 		struct sockaddr_in 	address;
 };
--- srcs/server/Server.cpp
+++ srcs/server/Server.cpp
@@ -12,13 +12,25 @@ Server::Server(const std::string & configFile) : _configFile(configFile), _socke
 Server::~Server() { std::cout << this->_configFile << std::endl; }
 
 void	Server::createSocket() {
-    if ((this->_socketFd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((this->_socketFd = socket(AF_INET, SOCK_STREAM, 0)) < 0) // Creation de la socket, retourne son FD
 		throw std::runtime_error("Erreur lors de la création du socket");
+	int flags = fcntl(this->_socketFd, F_GETFL, 0); // Recupere  le flag actuel de la socket
+    if (flags == -1) {
+        std::cerr << "Erreur fcntl(F_GETFL)" << std::endl;
+    }
+    if (fcntl(this->_socketFd, F_SETFL, flags | O_NONBLOCK) == -1) { // Ajoute le flag 0_NONBLOCK (devient non bloquant)
+        std::cerr << "Erreur fcntl(F_SETFL)" << std::endl;
+    }
+	std::cout << "Flag a la creation de la socket: " << flags << std::endl;
 	std::memset(&this->address, 0, sizeof(this->address));
-	this->address.sin_port = htons(8080);	// PORT
-	this->address.sin_family = AF_INET;		// IPV4 INTERNET PROTOCOL
-	this->address.sin_addr.s_addr = htonl(INADDR_ANY); // ADDRESS IP DE LA SOCKET (INADDR _ANY = l'os se debrouille)
-	if (bind(this->_socketFd, (struct sockaddr *)&this->address, sizeof(this->address)) < 0) // Bind la socket a l'interface reseau, la relie. On peut donc s'y connecter en  utilisant le bon port et recevoir des connexions entrantes. En gros c'est son adresse
+	// PORT
+	this->address.sin_port = htons(8080);
+	// IPV4 INTERNET PROTOCOL
+	this->address.sin_family = AF_INET;
+	// ADDRESS IP DE LA SOCKET (INADDR _ANY = l'os se debrouille)
+	this->address.sin_addr.s_addr = htonl(INADDR_ANY);
+	 // Bind la socket a l'interface reseau, la relie. On peut donc s'y connecter en  utilisant le bon port et recevoir des connexions entrantes. En gros c'est son adresse
+	if (bind(this->_socketFd, (struct sockaddr *)&this->address, sizeof(this->address)) < 0)
 	{
 		throw std::runtime_error("Bind failed");
 	}
@@ -36,14 +48,35 @@ void	Server::configSocket() {
 	// SOMAXCONN est la valeur maximale recommandée par le système (généralement 128)
 	if (listen(this->_socketFd, SOMAXCONN) < 0)
 		throw std::runtime_error("Échec de l'écoute sur le socket");
-	else
-		std::cout << "Socket en écoute avec succès" << std::endl;
+
+}
+
+void	Server::runServer() {
+	// On va attendre une nouvelle connexion entrante avec accept(). acept cree un nouveau socket
+	// specifique a cette connexion et return son FD
+	std::cout << "En attente de connexions..." << std::endl;
+	socklen_t addrlen = sizeof(this->address);
+	int		new_socket;
+
+	while (true)
+	{
+		new_socket = accept(this->_socketFd, (struct sockaddr *)&this->address, &addrlen);
+		if (new_socket < 0) {
+			std::cerr << "Accept eechoué" << std::endl;
+			continue;
+		}
+		std::cout << "Nouvelle connexion acceptée: " << new_socket << std::endl;
+		this->_connexions.push_back(new_socket);
+	}
+	close(this->_socketFd);
+	
 }
 
 void Server::init(std::string &configFile) {
 	this->_configFile = configFile;
 	createSocket();
 	configSocket();
+	runServer();
 }
 
 // Operators
