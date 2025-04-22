a8abe8be92fe79fb7e24359b246bf6756c99ec7e
Joaquim Pfeiffer <jopfeiff@paul-f4Ar6s9.clusters.42paris.fr>
2025-04-22 15:24:39 +0200

    const

--- deps/server_Request.d
+++
@@ -1,7 +0,0 @@
-objs/server_Request.o: srcs/server/Request.cpp \
-  srcs/server/../../includes/server/Request.hpp \
-  srcs/server/../../includes/server/Response.hpp
-
-srcs/server/../../includes/server/Request.hpp:
-
-srcs/server/../../includes/server/Response.hpp:
