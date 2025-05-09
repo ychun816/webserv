0a0e8b628698a54f2cd7e3a5ad5a3c5477298a25
ychun816 <105208178+ychun816@users.noreply.github.com>
2025-05-09 23:17:49 +0200

    starting post

--- includes/methods/Post.hpp
+++ includes/methods/Post.hpp
@@ -2,6 +2,11 @@
 
 #include "AMethods.hpp"
 
+/* GOAL:
+- Accepts data from clients (e.g., form submissions, file uploads)
+- Typically store data server-side
+*/
+
 class Post : public AMethods
 {
 	public:
@@ -10,7 +15,9 @@ class Post : public AMethods
 		// Post&	operator=(const Post& copy);
 		virtual ~Post();
 
-		// virtual void execute(Request& request, Response& response, Server& server);
+		virtual void execute(Request& request, Response& response, Server& server); //Best practice: make destructors virtual in base classes when the class is intended to be inherited.
 
 	private:
+		void handleUpload(Request& request, Response& response, Server& server);
+
 };
